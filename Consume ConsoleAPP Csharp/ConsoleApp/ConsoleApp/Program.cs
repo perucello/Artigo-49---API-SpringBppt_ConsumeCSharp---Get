49d491cc854cbdc53f767de0e9cfa6b1a1a1768f
perucello <perucello@hotmail.com>
2021-03-06 19:44:25 -0300

    EducaCiencia FastCode

---
+++ Consume ConsoleAPP Csharp/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net; //importar biblioteca Client
+using System.IO;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //Testando Console Basico
+            Console.WriteLine("Testando C# - EducaCiencia FastCode");
+            Console.ReadLine();
+            */
+
+            /*
+            //GetAPI_Java
+            GetAPI_Java();
+            Console.ReadKey();
+            */
+
+            
+            // GetAPI_Java_2
+            GetAPI_Java_2();
+            Console.ReadKey();
+
+        }
+
+
+        //Get API Java SpringBoot simples WebClient
+        public static void GetAPI_Java()
+        {
+            string endpointGet = "http://localhost:8080/api/JPA/clientes/";
+            WebClient api = new WebClient();
+            string content = api.DownloadString(endpointGet); //insere o endpoint Java
+            Console.WriteLine(content);
+        }
+
+        //Get API Java SpringBoot - WebRequest
+        public static void GetAPI_Java_2()
+        {
+            string endpointGet_2 = "http://localhost:8080/api/JPA/clientes/";
+            WebRequest request = WebRequest.Create(endpointGet_2);
+            request.Method = "GET";
+            var response = request.GetResponse();
+
+            if (((HttpWebResponse)response).StatusCode == HttpStatusCode.OK)
+            {
+                using (Stream stream = response.GetResponseStream())
+                {
+                    StreamReader streamReader = new StreamReader(stream);
+                    string content = streamReader.ReadToEnd();
+                    Console.WriteLine("GET" + content);
+                }
+            }
+            else
+            {
+                Console.WriteLine("GET Fail");
+            }
+        }
+
+        
+    }
+}
